7c36072b93c2f3783bb786ce354fe1647a7d888a
AustinRS016 <austinsantamaria@gmail.com>
2023-04-19 16:16:40 -0700

    apply preliminary filters for dataset

--- python/getHatcheryData.py
+++ python/getHatcheryData.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sodapy import Socrata
 import os
 from dotenv import load_dotenv
+from filterHatcheryData import *
 
 load_dotenv()
 
@@ -12,8 +13,9 @@ client = Socrata("data.wa.gov",
 
 # Limit defaults to 1000, not sure what to put for 'no-limit' dataset is ~450,000 rows
 #  use limit=1 for testing purposes and limit=10000000 for development
-results = client.get("9q4e-xhag", limit=1)
+results = client.get("9q4e-xhag", limit=1000)
 
 results_df = pd.DataFrame.from_records(results)
 
-# print(results_df.head(5))
+# Save this data to our DB
+final_df = filterHatcheryData(results_df)
