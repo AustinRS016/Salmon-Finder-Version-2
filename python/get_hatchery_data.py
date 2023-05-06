f9c82490e6937b44aa8a93d928f0c47257a2cd6d
AustinRS016 <austinsantamaria@gmail.com>
2023-05-05 21:02:19 -0700

    changes for new code

--- python/get_hatchery_data.py
+++ python/get_hatchery_data.py
@@ -37,7 +37,7 @@ class WDFWProviderLogic(HatcheryProviderLogic):
         #  use limit=1 for testing purposes and limit=10000000 for development
         results = client.get(
             "9q4e-xhag",
-            limit=100,
+            limit=100000000,
             select="species, origin, run, facility, adult_count, date",
             where=f"""
                 facility='{hatchery}'
@@ -53,51 +53,54 @@ class WDFWProviderLogic(HatcheryProviderLogic):
                     OR species='Sockeye'
                 )""",
         )
+        print(results)
+        # df = WDFW_to_df(results)
         return results
 
 
-def get_WDFW_Data(hatchery, date=""):
-    """
-    Example inputs:    
-    Parameters: 
-        hatchery: string
-            The name of the hatchery from the config
-        date: string
-            Optional parameter to query from a specific date
-            Format: YYYY-MM-DD
-    Returns:
-        hatcheryData: list of dictionaries for each row in dataset
-    """
-    if (date != ""):
-        date_query = f"AND date > '{date}'"
-    else:
-        date_query = ""
+# def get_WDFW_Data(hatchery, date=""):
+#     """
+#     Example inputs:    
+#     Parameters: 
+#         hatchery: string
+#             The name of the hatchery from the config
+#         date: string
+#             Optional parameter to query from a specific date
+#             Format: YYYY-MM-DD
+#     Returns:
+#         hatcheryData: list of dictionaries for each row in dataset
+#     """
+#     if (date != ""):
+#         date_query = f"AND date > '{date}'"
+#     else:
+#         date_query = ""
 
-    client = Socrata("data.wa.gov",
-                     os.getenv('APP_KEY'),
-                     username=os.getenv('APP_USERNAME'),
-                     password=os.getenv('APP_PASSWORD'))
+#     client = Socrata("data.wa.gov",
+#                      os.getenv('APP_KEY'),
+#                      username=os.getenv('APP_USERNAME'),
+#                      password=os.getenv('APP_PASSWORD'))
 
-    # Limit defaults to 1000, not sure what to put for 'no-limit' dataset is ~450,000 rows
-    #  use limit=1 for testing purposes and limit=10000000 for development
-    results = client.get("9q4e-xhag", limit=1000000000, select="species, origin, run, facility, adult_count, date",
-                         where=f"""
-                        facility='{hatchery}' 
-                        AND event='Trap Estimate'
-                        AND adult_count > 0 
-                        {date_query}
-                        AND (
-                            species='Coho'
-                            OR species='Chinook' 
-                            OR species='Steelhead'
-                            OR species='Chum' 
-                            OR species='Pink' 
-                            OR species='Sockeye' 
-                            )"""
-                         )
-    return results
+#     # Limit defaults to 1000, not sure what to put for 'no-limit' dataset is ~450,000 rows
+#     #  use limit=1 for testing purposes and limit=10000000 for development
+#     results = client.get("9q4e-xhag", limit=1000000000, select="species, origin, run, facility, adult_count, date",
+#                          where=f"""
+#                         facility='{hatchery}' 
+#                         AND event='Trap Estimate'
+#                         AND adult_count > 0 
+#                         {date_query}
+#                         AND (
+#                             species='Coho'
+#                             OR species='Chinook' 
+#                             OR species='Steelhead'
+#                             OR species='Chum' 
+#                             OR species='Pink' 
+#                             OR species='Sockeye' 
+#                             )"""
+#                          )
+#     return results
 
 def WDFW_to_df(results):
+
     '''
     Paramters:
         results: list of dictionaries for each row in the dataset
@@ -105,6 +108,7 @@ def WDFW_to_df(results):
         df: pandas dataframe
     '''
     df = pd.DataFrame(results)
+    print(df)
     df.drop_duplicates()
     df['date'] = pd.to_datetime(df['date'])
     df['adult_count'] = pd.to_numeric(df['adult_count'])
--- python/runner.py
+++ python/runner.py
@@ -1,8 +1,26 @@
 from config import hatcheries
-from get_hatchery_data import WDFWProviderLogic
+from get_hatchery_data import WDFWProviderLogic, WDFW_to_df
+from create_graph_data import find_distinct_populations, compute_bargraph_data, get_rolling_average
 
 wdfw_provider_logic = WDFWProviderLogic()
 
+print(wdfw_provider_logic)
+x = 0
 for hatchery_name, provider in hatcheries.items():
-    data = provider.hatchery_provider.value.get_fish_data(hatchery_name)
-    print(data)
+    if x > 1:
+        break
+    df = provider.hatchery_provider.value.get_fish_data(hatchery_name)
+    print(df)
+
+    distinct_populations = find_distinct_populations(df)
+
+    # Outputs as JSON
+    bargraph = compute_bargraph_data(df, distinct_populations)
+    rolling_average = get_rolling_average(df, distinct_populations)
+
+    with open (f"{hatchery_name}_bargraph.json", "w") as outfile:
+        outfile.write(bargraph)
+
+    with open (f"{hatchery_name}_areagraph.json", "w") as outfile:
+        outfile.write(rolling_average)
+    x = x + 1
